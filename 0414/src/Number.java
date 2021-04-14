902f15474450a1dc8e72793fbdf8423aec01d0fc
wh <uckuck99@naver.com>
2021-04-14 15:18:09 +0900

    0414 추가

---
+++ 0414/src/Number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Number {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("10개의 랜덤 숫자 중 하나를 맞춰보세요(1~50) : ");
+		int b = sc.nextInt();
+		int c=0;
+		for(int i=0;i<10;i++) {
+			int a = (int)(Math.random()*50+1);
+		
+			System.out.print(a+" ");
+			if(b==a){
+				c++;
+			}
+		}
+		
+		if(c==1) {
+			System.out.println("\n"+"성공");
+		}else System.out.println("\n"+"꽝");
+		
+
+	}
+
+}
